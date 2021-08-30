5b4c48ac501e790290eefd6fa13e43b781cc2fef
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2021-08-30 11:09:05 -0300

    Commit api_marvel

--- src/services/api_marvel.ts
+++ src/services/api_marvel.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import md5 from "md5";
 
-export const publicKey = 'da51eb2da6eeb9a915df90822003e27c';
-const privateKey = '86371ef198197d8d497240c62452ba4292267d38';
+export const publicKey = '[sua publicKey]';
+const privateKey = '[sua privateKey]';
 export const time = Number(new Date());
 export const hash = md5(time + privateKey + publicKey);
 
